bbacde3bd703ad68b11c21b971e159b06745a8d9
apeTrackunit <ape@trackunit.com>
2022-12-07 15:25:00 +0100

    Fixes

--- Sep3PresentationTier/Model/Location.cs
+++ Sep3PresentationTier/Model/Location.cs
@@ -2,23 +2,7 @@ namespace Model;
 
 public class Location
 {
-    public double latitude { get; set; }
-    public double longitude { get; set; }
-    public int size { get; set; }
-
-    public Location(double latitude, double longitude, int size)
-    {
-        latitude = latitude;
-        longitude = longitude;
-        size = size;
-    }
-
-    public Location(int size)
-    {
-        size = size;
-    }
-
-    public Location()
-    {
-    }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public int Size { get; set; }
 }
