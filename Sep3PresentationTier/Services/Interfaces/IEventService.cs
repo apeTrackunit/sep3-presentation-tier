dd87eae4b7b3e61546918eb6da39e12b6a818c7c
apeTrackunit <ape@trackunit.com>
2022-12-10 18:21:03 +0100

    Init commit

--- Sep3PresentationTier/Services/Interfaces/IEventService.cs
+++ Sep3PresentationTier/Services/Interfaces/IEventService.cs
@@ -5,4 +5,6 @@ using Model;
 public interface IEventService
 {
     Task<bool> CreateAsync(Event cleaningEvent);
+    Task<bool> ApproveEvent(string id, bool approved);
+    Task<Event> GetEvent(string id);
 }
